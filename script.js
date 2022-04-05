a52142d1e286507605ef5942008d18c50b45f391
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-04-05 19:31:45 -0300

    4th: Added read button & functionality on JS.

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-const book1 = {
-    title: "Harry Potter",
-    author: "JK Rowling",
-    pages: 350,
-}
-
-const book2 = {
-    title: "Naruto",
-    author: "Kishimoto",
-    pages: 40,
-}
-
-let myLibrary = [book1, book2];
+let myLibrary = [];
 
 // OBJECT BOOK CONSTRUCTOR
-function bookGenerator(title, author) {
+function bookGenerator(title, author, read) {
     this.title = title,
-    this.author = author;
+    this.author = author,
+    this.read = read;
 }
 
-// FUNCTION FOR ADDING A BOOK TO ARRAY
-
+// FUNCTION TO ADD A BOOK & DISPLAY IT
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
+const read = document.querySelector('.checkbox');
+const maincontent = document.querySelector('.maincontent');
 
 function addBookToLibrary() {
-    let book = new bookGenerator(title.value, author.value);
+    let book = new bookGenerator(title.value, author.value, read.checked);
     myLibrary.push(book);
-    console.log(myLibrary);
+    const card = document.createElement('div');
+    card.classList.add('card');
+    if (read.checked === true) {
+        card.classList.add('yes');
+    } else {
+        card.classList.add('no');
+    }
+    card.textContent = myLibrary[myLibrary.length - 1]['title'] + ' by ' + myLibrary[myLibrary.length - 1]['author'];
+    maincontent.appendChild(card);
 }
 
 document.querySelector('.addbutton').addEventListener('click', addBookToLibrary);
-console.log(myLibrary);
-
-// Loop myLibrary & Display on maincontent
-
